8e87521759a3f383cd175265cb4cfa9f147d9788
Touré Holder <toureholder@gmail.com>
2021-07-01 22:08:26 -0300

    chore: create movie list page

---
+++ lib/pages/movie_list_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../model/movie.dart';
+
+class MovieListPage extends StatelessWidget {
+  const MovieListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final movies = Movie.getList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movies'),
+      ),
+      body: MovieListView(
+        movies: movies,
+        onTapItem: (Movie movie) {
+          // TODO: Navigate to detail screen
+        },
+      ),
+    );
+  }
+}
+
+class MovieListView extends StatelessWidget {
+  final List<Movie> movies;
+  final Function(Movie) onTapItem;
+
+  const MovieListView({
+    Key? key,
+    required this.movies,
+    required this.onTapItem,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+        return ListTile(
+          key: Key('list_item_$index'),
+          leading: Image.network(SMALL_POSTER_BASE_URL + movie.posterPath),
+          title: Text('${movie.title}'),
+          contentPadding: EdgeInsets.all(12.0),
+          onTap: () {
+            onTapItem.call(movie);
+          },
+        );
+      },
+    );
+  }
+}
